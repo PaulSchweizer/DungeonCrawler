09e9ee0e4314e04904698e7abcda4026b9b066da
Paul Schweizer <paulschweizer@gmx.net>
2017-02-15 22:14:38 -0800

    updates

---
+++ DungeonCrawler/Core/SpreadsheetHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using DungeonCrawler.Items;
+using System.Data;
+
+namespace DungeonCrawler.Core
+{
+    public static class SpreadsheetHandler
+    {
+
+        public static string AppUrl = "https://script.google.com/macros/s/AKfycbyhrsOHHB9ilJ_9Y9E1XzfilzXeTMt6alkfHUwW1G-N2CuINIg/exec";
+        public static string RegisteredGames = "table=RegisteredGames";
+        public static string Attributes = "table=Attributes";
+        public static string QuestTypes = "table=QuestTypes";
+        public static string Events = "table=Events";
+        public static string Tiles = "table=Tiles";
+
+        public static string Get(string data)
+        {
+            using (WebClient client = new WebClient())
+            {
+                var url = string.Concat(AppUrl, "?", data);
+                var json = client.DownloadString(url);
+                return json;
+            }
+        }
+
+        public static DataSet GetRegisteredGames()
+        {
+            var json = Get(RegisteredGames);
+            DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(json);
+            return dataSet;
+        }
+
+    }
+}
---
+++ DungeonCrawler/NUnit/SpreadsheetHandlerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using DungeonCrawler.Core;
+
+namespace DungeonCrawler.NUnit
+{
+    [TestFixture]
+    class SpreadsheetHandlerTest
+    {
+        [Test]
+        public void LoadRegisteredGames()
+        {
+            var data = SpreadsheetHandler.GetRegisteredGames();
+            Console.WriteLine(data.Tables["Table"].Rows[0]["Name"]);
+        }
+    }
+}
